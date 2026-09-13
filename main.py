c9ca57fe87d5e19493b49b41b93caf137e077da4
itsmegift <itsmegift@gmail.com>
2026-09-13 15:50:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,35 +4,42 @@
 # 3. Update the SMTP ADDRESS to match your email provider.
 # 4. Update birthdays.csv to contain today's month and day.
 # See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
-import os
-
 # import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+email = os.environ.get("MY_EMAIL")
+password = os.environ.get("MY_PASSWORD")
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+import smtplib
+import datetime as dt
+from random import choice
+import pandas
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+def send_email(message):
+    with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+        connection.login(user=email,password=password)
+        connection.sendmail(from_addr=email,
+                            to_addrs="itsmegift@gmail.com",
+                            msg=f"Subject:Happy Birthday!!! \n\n {message}"
+                        )
+
+#Choose the Email Template and change name
+def create_email(name):
+    template_no = choice([1, 2, 3])
+    with open(f"letter_templates/letter_{template_no}.txt") as template:
+        email_text = template.read()
+        updated_email_text = email_text.replace("[NAME]",name)
+        send_email(updated_email_text)
+
+# READ THE CSV FILE
+birthday_df = pandas.read_csv("birthdays.csv")
+birthday_dict = birthday_df.to_dict(orient="records")
+
+now = dt.datetime.now()
+
+for person in birthday_dict:
+    if person["month"] == now.month and person["day"] == now.day:
+        create_email(person["name"])
+
+date_month_tuple = {(row.day,row.month): row for (index,row) in birthday_df.iterrows()}
+print(date_month_tuple)
